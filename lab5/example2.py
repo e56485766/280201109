1807bd9073f387be152a3acd0e758a979f75a215
e56485766 <ege_yucel_90@hotmail.com>
2020-11-16 09:20:29 +0000

    lab5 example2

--- lab5/example2.py
+++ lab5/example2.py
@@ -1,14 +1,11 @@
 n = int(input("How many numbers? "))
 
 even = 0
-odd = 0
 
 for i in range(n):
   number = int(input("Enter an integer: "))
   if number % 2 == 0:
     even += 1
-  else:
-    odd += 1
 
 percentage = even / n * 100
 print(str(percentage) + "%")
