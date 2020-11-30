9f88d42461748121cc8de71074879033e2288d28
e56485766 <ege_yucel_90@hotmail.com>
2020-11-30 09:10:55 +0000

    lab7 example2 (cont.)

--- lab7/example2.py
+++ lab7/example2.py
@@ -2,8 +2,9 @@ books = ["ULYSSES","ANIMAL FARM","BRAVE NEW WORLD","ENDER'S GAME"]
 
 book_dict = {}
 
-for book in books:
+for i in books:
   unique_char = set(i)
-  book_dict[i] = (len(i), len(unique_char))
+  average = (len(i) + len(unique_char)) / 2
+  book_dict[i] = (len(i), len(unique_char), average)
 
 print(book_dict)
