757b77508ec2fd61a8234444cf324a9d42df7f68
e56485766 <ege_yucel_90@hotmail.com>
2020-12-14 09:20:21 +0000

    lab8 example2

---
+++ lab8/example2.py
@@ -0,0 +1,12 @@
+def is_prime(number):
+  prime = True
+  for i in range(2,number):
+    if number % i ==0:
+      prime = False
+
+  return prime
+        
+print(is_prime(2))
+print(is_prime(3))
+print(is_prime(10))
+print(is_prime(11))
